3a5a4d89bb43efd17469241a6b088b5fa6ae3628
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-04-13 20:52:37 +0800

    Use nur-template

--- flake.nix
+++ flake.nix
@@ -1,37 +1,9 @@
 {
-  description = "A NixOS flake which try to impl end-4_dots-hyprland in nixos";
-
-  outputs = { self, impurity, ... }@inputs: {
-    # editing flake.nix triggers certain utilities such as direnv
-    # to reload - editing host configurations do not require a direnv
-    # reload, so lets move hosts out of the way
-
-    nixosModules = {
-      "end_4-dots_hyprland" = {
-        imports = [ ./modules/nixos ];
-      };
-    };
-    homeManagerModules = {
-      "end_4-dots_hyprland" = {
-        imports = [ ./modules/hm ];
-      };
-    };
-  };
+  description = "A nur flake which try to impl end-4_dots-hyprland in nixos";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     impurity.url = "github:outfoxxed/impurity.nix";
     thorium.url = "github:end-4/nix-thorium";
-
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      # inputs.nixpkgs.follows = "hyprland";
-    };
-
     ags.url = "github:Aylur/ags";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
@@ -54,14 +26,41 @@
       url = "github:rafaelmardojai/firefox-gnome-theme";
       flake = false;
     };
-    anyrun = {
-      url = "github:Kirottu/anyrun";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     end_4-dots_hyprland = {
       url = "github:end-4/dots-hyprland";
       flake = false;
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      # inputs.nixpkgs.follows = "hyprland";
+    };
+    anyrun = {
+      url = "github:Kirottu/anyrun";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "armv6l-linux"
+        "armv7l-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      legacyPackages = forAllSystems (system: import ./default.nix {
+        pkgs = import nixpkgs { inherit system; };
+      });
+      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+    };
 }
